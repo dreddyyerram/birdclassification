8197e45b83ea2c9f7966fec124cd9985d84a8a29
bablooyerram <30732389+bablooyerram@users.noreply.github.com>
2020-03-28 16:08:18 +0530

    Update featureextraction.py

--- featureextraction.py
+++ featureextraction.py
@@ -1,47 +1,52 @@
-import librosa
+import librosa #a python package for music and sound analysis
 import numpy
 import pandas as pd
-import matplotlib
 import os
-import librosa
 import csv
-from sklearn.model_selection import train_test_split
 
-directory=('C:/Users/hp/PycharmProjects/untitled1/wavfiles')
-feature_matrix=numpy.empty([1,14])
-dataset=pd.DataFrame()
+
+directory=('C:/Users/hp/PycharmProjects/untitled1/wavfiles') #Specifying path to wavfiles directory
+
+dataset=pd.DataFrame()#creating an empty dataframe
+''' The below loop opens birdsong_metadata.csv file and takes the file_id and 
+the name of corresponding species stores it in a 'names' dictionary '''
 id=1
 names={}
 filename = "birdsong_metadata.csv"
 with open(filename, 'r') as csvfile:
     csvreader = csv.reader(csvfile)
 
-    for row in csvreader:
-        nam = "xc" + row[1] + ".wav"
-        names[nam]=row[3]
+    for row in csvreader: #each row in the csv file
+        nam = "xc" + row[1] + ".wav"  #row[1] is file id which is appended with xc and used it as a key for dictionary
+        names[nam]=row[3] #row[3] is the corresponding species name
 
 
-for i in os.listdir(directory):
+
+for i in os.listdir(directory): #for each wav file 
     print(i)
 
-    audiosamples, samplerate=librosa.load('C:/Users/hp/PycharmProjects/untitled1/wavfiles/'+i)
-    audiosamples=librosa.resample(audiosamples, samplerate, 8000)
+    audiosamples, samplerate=librosa.load('C:/Users/hp/PycharmProjects/untitled1/wavfiles/'+i) #loading the audio file
+    audiosamples=librosa.resample(audiosamples, samplerate, 8000) # resampling audio to a fixed sample rate
     mfcc_features=librosa.feature.mfcc(y=audiosamples,sr= 8000, n_mfcc=13)
-    print(mfcc_features.shape[1])
-    a=numpy.transpose(mfcc_features)
-    na=names[i]
-    ad = numpy.full(( mfcc_features.shape[1],1), na)
+   
+    a=numpy.transpose(mfcc_features) #transposing the mfcc_features 
+    na=names[i] #getting name of the species of that wav
+    ad = numpy.full(( mfcc_features.shape[1],1), na)#mfcc_features.shape[1] tells the no of times mfcc features were extracted
+    print(ad)
     df1=pd.DataFrame(ad)
-    df = pd.DataFrame(a)
-    df['label'] = df1
-    if id == 1:
-        dataset=df.copy()
+    df = pd.DataFrame(a)#this data frame contains all feature vectors extracted from the present wav file
+    df['label'] = df1 #this dataframe contains the same name of the species repeated 'mfcc_features.shape[1]' times
+    if id == 1: # this block satisfies only once in the begining
+        dataset=df.copy() #to intialize the dataset for the first time. This line runs only once at the begining and then id set to 0
         id=0
     else:
-        dataset=pd.concat([dataset,df])
-    print(dataset)
+        dataset=pd.concat([dataset,df]) #appends the dataset with feature vectors and label of the present wavfile(so, all the vectors that are taken from same wav file will have the same label)
 
-dataset.to_csv('extractedfeatures.csv')
+dataset.to_csv('extractedfeatures.csv') #output it to csv file
+
+
+
+
 
 
 
